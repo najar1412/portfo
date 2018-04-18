620c7c15f88872a74dd27176f28bcc084c4bfcd0
najar1412 <rory.jarrel@gmail.com>
2018-04-18 11:23:19 -0400

    refactored logic in views to module.utilities

--- portfo/app.py
+++ portfo/app.py
@@ -8,6 +8,7 @@ from module import model
 from forms import LoginForm, RegistrationForm, UploadForm
 from config import Config
 import flaskapp
+from module.utilities import ManageImage, ManageFolio
 
 
 login_manager = LoginManager(flaskapp.app)
@@ -18,6 +19,7 @@ login_manager.login_view = 'login'
 def load_user(id):
     return model.User.query.get(int(id))
 
+
 def first_time_run():
     users = model.User.query.all()
     if len(users) == 0:
@@ -25,134 +27,6 @@ def first_time_run():
 
     else:
         return False
-
-
-def new_image(session, filename=None, name=None, caption=None, date=None, featured=False, private=True):
-    image = model.Image(name=str(name), caption=str(caption), date=str(date), featured=featured, filename=str(filename), private=private)
-    session.session.add(image)
-    session.session.commit()
-
-    return image
-
-
-def delete_image_from_server(filename):
-    try:
-        os.remove(os.path.join(Config.UPLOAD_PATH, filename))
-    except FileNotFoundError as e:
-        print(e)
-
-
-def delete_image(row):
-    delete_image_from_server(row.filename)
-
-    return row
-
-
-def _image_to_dict(Image):
-    image = {
-        'id': Image.id,
-        'name': Image.name,
-        'caption': Image.caption,
-        'date': Image.date,
-        'featured': Image.featured,
-        'filename': Image.filename,
-        'private': Image.private
-    }
-
-    return image
-
-
-def get_images(private=None):
-    images = {}
-    for image in model.Image.query.all():
-        if private:
-            if image.private:
-                images[image.id] = _image_to_dict(image)
-
-        if private == False:
-            if image.private == False:
-                images[image.id] = _image_to_dict(image)
-
-        if private == None:
-            images[image.id] = _image_to_dict(image)
-
-    return images
-
-
-def image_by_id(id):
-    return model.Image.query.filter_by(id=id).first()
-
-
-def folio_by_id(id):
-    return model.Folio.query.filter_by(id=id).first()
-
-
-def db_edit_image(session, id, dto_image):
-    image = model.Image.query.filter_by(id=id).first()
-
-    if 'image_name' in dto_image and dto_image['image_name']:
-        image.name = dto_image['image_name']
-
-    if 'image_caption' in dto_image and dto_image['image_caption']:
-        image.caption = dto_image['image_caption']
-
-    if 'image_date' in dto_image and dto_image['image_date']:
-        image.date = dto_image['image_date']
-
-    if 'image_featured' in dto_image and dto_image['image_featured']:
-        if dto_image['image_featured'] == 'True':
-            image.featured = True
-
-        elif dto_image['image_featured'] == 'False':
-            image.featured = False
-
-    if 'image_private' in dto_image and dto_image['image_private']:
-        if dto_image['image_private'] == 'True':
-            image.private = True
-
-        elif dto_image['image_private'] == 'False':
-            image.private = False
-
-    if 'delete_image' in dto_image and dto_image['delete_image'] == 'True':
-        session.session.delete(delete_image(image))
-        session.session.commit()
-
-        return True
-    
-    session.session.commit()
-
-
-    return image
-
-
-def db_edit_folio(s, id, dto_folio):
-    folio = folio_by_id(id)
-    print(dto_folio)
-
-    if 'folio_title' in dto_folio and dto_folio['folio_title'] and dto_folio['folio_title'] != '':
-        folio.title = dto_folio['folio_title']
-    if 'folio_caption' in dto_folio and dto_folio['folio_caption'] and dto_folio['folio_caption'] != '':
-        folio.caption = dto_folio['folio_caption']
-    if 'folio_title_enable' in dto_folio and dto_folio['folio_title_enable'] == 'True':
-        if folio.enable_title == False:
-            folio.enable_title = True
-            session['portfo_title_enable'] = True
-        else:
-            folio.enable_title = False
-            session['portfo_title_enable'] = False
-        
-
-    if 'folio_caption_enable' in dto_folio and dto_folio['folio_caption_enable'] == 'True':
-        if folio.enable_caption == False:
-            folio.enable_caption = True
-            session['portfo_caption_enable'] = True
-        else:
-            folio.enable_caption = False
-            session['portfo_caption_enable'] = False
-    
-    s.session.commit()
-
-    return folio
 
 
 def portfo_cookie(session, portfo_title, portfo_caption, portfo_title_enable, portfo_caption_enable):
@@ -174,9 +48,8 @@ def register():
         user = model.User(username=form.username.data, email=form.email.data)
         user.set_password(form.password.data)
 
-        folio = model.Folio()
+        folio = ManageFolio(flaskapp.db).new()
         flaskapp.db.session.add(user)
-        flaskapp.db.session.add(folio)
         flaskapp.db.session.commit()
 
         portfo_cookie(session, folio.title, folio.caption, folio.enable_title, folio.enable_caption)
@@ -195,8 +68,8 @@ def register():
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
-    form = LoginForm()
 
+    form = LoginForm()
     if form.validate_on_submit():
         user = model.User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
@@ -205,6 +78,7 @@ def login():
             return redirect(url_for('login'))
 
         login_user(user, remember=form.remember_me.data)
+
         return redirect(url_for('admin'))
 
     return render_template('login.html', title='Sign In', form=form)
@@ -219,17 +93,17 @@ def logout():
 
 @flaskapp.app.route('/')
 def index():
+    # TODO: refactor cookie session
     if first_time_run():
-        print('its true')
-
         return redirect('/register')
 
     else:
-        folio = model.Folio.query.filter_by(id=1).first()
+        # folio = model.Folio.query.filter_by(id=1).first()
+        folio = ManageFolio().get_by_id(id=1)
         session['portfo_title'] = folio.title
         session['portfo_caption'] = folio.caption
         
-        images = get_images(private=False)
+        images = ManageImage().get_all(private=False)
 
         return render_template('index.html', images=images)
 
@@ -238,20 +112,22 @@ def index():
 @login_required
 def admin():
     form = UploadForm()
-    images = get_images()
+    images = ManageImage().get_all()
 
     return render_template('admin.html', images=images, form=form)
 
 
 @flaskapp.app.route('/upload', methods=['GET', 'POST'])
 def upload():
+    # TODO: refactor upload code to utilities
     form = UploadForm()
     if form.validate_on_submit() and 'photo' in request.files:
         for f in request.files.getlist('photo'):
             filename = secure_filename(f.filename)
             f.save(os.path.join(flaskapp.app.config['UPLOAD_PATH'], filename))
-            new_image(flaskapp.db, filename=filename)
-        return 'Upload completed.'
+            ManageImage(flaskapp.db).new(filename=filename)
+
+        return redirect('/admin')
 
     return render_template('upload.html', form=form)
 
@@ -259,7 +135,7 @@ def upload():
 @flaskapp.app.route('/admin/edit/<int:id>', methods=['GET', 'POST'])
 def admin_edit_image(id):
     form = request.form
-    db_edit_image(flaskapp.db, id, form)
+    ManageImage(flaskapp.db).edit(id, form)
 
     return redirect('/admin')
 
@@ -267,7 +143,7 @@ def admin_edit_image(id):
 @flaskapp.app.route('/admin/edit/folio/<int:id>', methods=['GET', 'POST'])
 def admin_edit_folio(id):
     form = request.form
-    db_edit_folio(flaskapp.db, id, form)
+    ManageFolio(flaskapp.db).edit(session, id, form)
 
     return redirect('/admin')
 
---
+++ portfo/module/utilities.py
@@ -0,0 +1,202 @@
+import os
+
+from module import model
+from config import Config
+
+class ManageImage():
+    def __init__(self, db_session=None):
+        self.db_session = db_session
+
+
+    def _delete_image_from_server(self, filename):
+        """deletes a file based on filename from flask upload dir
+        filename: str, filename of image to delete
+        
+        return: boolean"""
+        try:
+            os.remove(os.path.join(Config.UPLOAD_PATH, filename))
+            return True
+
+        except FileNotFoundError as e:
+            print(e)
+            return False
+
+
+    def _image_to_dict(self, obj):
+        """returns dict repr of database row
+        obj: sqlalchemy Image object
+        """
+        return {
+            'id': obj.id,
+            'name': obj.name,
+            'caption': obj.caption,
+            'date': obj.date,
+            'featured': obj.featured,
+            'filename': obj.filename,
+            'private': obj.private
+        }
+
+
+    def new(self, filename=None, name=None, caption=None, date=None, featured=False, private=True):
+        """creates a new database entry for an image
+        filename: str, name of file and extension
+        name: str, name of image
+        caption: str, description of image
+        date: NOT IMP
+        featured: boolean, featured image
+        private: boolean, public or not"""
+        image = model.Image(name=str(name), caption=str(caption), date=str(date), featured=featured, filename=str(filename), private=private)
+
+        self.db_session.session.add(image)
+        self.db_session.session.commit()
+
+        return image
+
+
+    def delete_image(self, obj):
+        """deletes image information from app
+        obj: sqlalchemy Image object
+        
+        return: boolean"""
+        if self._delete_image_from_server(obj.filename):
+            self.db_session.session.delete(obj)
+            self.db_session.session.commit()
+
+            return True
+
+        else:
+            print('deleting image failed')
+
+            return False
+
+    
+    def image_by_id(self, id):
+        """queries database for a single row object
+        id: int, id of row
+        
+        return: sqlalchemy Image object"""
+        return model.Image.query.filter_by(id=id).first()
+
+
+    def edit(self, id, dto_image):
+        """handles editing images
+        id: int, id of image to edit
+        dto_image: dict, data to update image with
+
+        return: sqlalchemy Image object"""
+        image = model.Image.query.filter_by(id=id).first()
+
+        if 'image_name' in dto_image and dto_image['image_name']:
+            image.name = dto_image['image_name']
+
+        if 'image_caption' in dto_image and dto_image['image_caption']:
+            image.caption = dto_image['image_caption']
+
+        if 'image_date' in dto_image and dto_image['image_date']:
+            image.date = dto_image['image_date']
+
+        if 'image_featured' in dto_image and dto_image['image_featured']:
+            if dto_image['image_featured'] == 'True':
+                image.featured = True
+
+            elif dto_image['image_featured'] == 'False':
+                image.featured = False
+
+        if 'image_private' in dto_image and dto_image['image_private']:
+            if dto_image['image_private'] == 'True':
+                image.private = True
+
+            elif dto_image['image_private'] == 'False':
+                image.private = False
+
+        if 'delete_image' in dto_image and dto_image['delete_image'] == 'True':
+            try:
+                self.delete_image(image)
+            except:
+                print('Image information not deleted')
+        
+        self.db_session.session.commit()
+
+
+        return image
+
+
+    def get_all(self, private=None):
+        """retrieves all image objs from database
+        private: boolean, filters results
+        
+        return: dict, dict repr of objects"""
+        images = {}
+        for image in model.Image.query.all():
+            if private:
+                if image.private:
+                    images[image.id] = self._image_to_dict(image)
+
+            if private == False:
+                if image.private == False:
+                    images[image.id] = self._image_to_dict(image)
+
+            if private == None:
+                images[image.id] = self._image_to_dict(image)
+
+        return images
+
+
+class ManageFolio():
+    def __init__(self, db_session=None):
+        self.db_session = db_session
+
+    def new(self):
+        """creates a new folio object in the database
+        
+        return: sqlalchemy Folio object"""
+        folio = model.Folio()
+        self.db_session.session.add(folio)
+        self.db_session.session.commit()
+
+        return folio
+
+
+    def get_by_id(self, id):
+        """retrieves sqlalchemy Folio object
+        id: int, id of object
+        
+        return: sqlalchemy Folio object"""
+        return model.Folio.query.filter_by(id=id).first()
+
+
+    def edit(self, flask_session, id, dto_folio):
+        """handles editing of sqlalchemy Folio object
+        id: int, id of object to edit
+        dto_folio: dict, edits to make
+        
+        return: sqlalchemy Folio object"""
+        # TODO: Imp a class for handling flask session object
+        folio = self.get_by_id(id)
+
+        if 'folio_title' in dto_folio and dto_folio['folio_title'] and dto_folio['folio_title'] != '':
+            folio.title = dto_folio['folio_title']
+
+        if 'folio_caption' in dto_folio and dto_folio['folio_caption'] and dto_folio['folio_caption'] != '':
+            folio.caption = dto_folio['folio_caption']
+
+        if 'folio_title_enable' in dto_folio and dto_folio['folio_title_enable'] == 'True':
+            if folio.enable_title == False:
+                folio.enable_title = True
+                flask_session['portfo_title_enable'] = True
+            else:
+                folio.enable_title = False
+                flask_session['portfo_title_enable'] = False
+            
+
+        if 'folio_caption_enable' in dto_folio and dto_folio['folio_caption_enable'] == 'True':
+            if folio.enable_caption == False:
+                folio.enable_caption = True
+                flask_session['portfo_caption_enable'] = True
+            else:
+                folio.enable_caption = False
+                flask_session['portfo_caption_enable'] = False
+        
+        self.db_session.session.commit()
+
+        return folio
